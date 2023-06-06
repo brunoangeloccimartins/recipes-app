23b5e8de0c9173c7fb34623a0d97721ebfcc4120
Joatan Feitosa <joatancarlos@gmail.com>
2023-06-06 14:58:40 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Recipes from './pages/Recipes/Recipes';
+import Profile from './components/Profile';
+import Done from './components/Done';
+import Favorites from './components/Favorites';
 
 function App() {
   return (
@@ -11,9 +14,17 @@ function App() {
         <Route exact path="/" component={ Login } />
         <Route path="/meals" component={ Recipes } />
         <Route path="/drinks" component={ Recipes } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/done-recipes" component={ Done } />
+        <Route path="/favorite-recipes" component={ Favorites } />
       </Switch>
     </main>
   );
 }
 
 export default App;
+
+// Rota "/meals/:id-da-receita": não deve ter footer
+// Rota "/drinks/:id-da-receita": não deve ter footer
+// Rota "/meals/:id-da-receita/in-progress": não deve ter footer
+// Rota "/drinks/:id-da-receita/in-progress": não deve ter footer
