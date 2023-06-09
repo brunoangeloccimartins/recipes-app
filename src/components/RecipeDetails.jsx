33fc6a3b86f57c4c2e3066d97fb250a8af324333
Danillo Santos <dnashy@hotmail.com>
2023-06-09 17:46:17 -0300

    Desenvolvimento da página de Receitas Favoritas

---
+++ src/components/RecipeDetails.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+// import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+// import useFetch from '../services/hooks/useFetch';
+// import { fetchMealsById, fetchDrinksById } from '../services/fetchRequisition';
+
+function RecipeDetails() {
+  // const history = useHistory();
+  // const { fetchData } = useFetch();
+  // const { location: { pathname } } = history;
+  // const [meal, setMeal] = useState('');
+  // const [drink, setDrink] = useState('');
+
+  // const requestDetails = async () => {
+  //   const pathnameArray = pathname.split('s/');
+  //   const recipeType = pathnameArray[0];
+  //   const id = pathnameArray[1];
+
+  //   if (recipeType === '/meal') {
+  //     const mealRequest = await fetchData(fetchMealsById(id));
+  //   }
+  //   if (recipeType === '/drink') {
+  //     const drinkRequest = await fetchData(fetchDrinksById(id));
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   requestDetails();
+  // }, []);
+
+  return (
+    <>
+      Olá
+    </>
+  );
+}
+
+//
+export default RecipeDetails;
---
+++ src/pages/FavoriteRecipes/FavoriteRecipes.jsx
@@ -0,0 +1,156 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import Button from '../../components/Button';
+import Header from '../../components/Header/Header';
+import shareIcon from '../../images/shareIcon.svg';
+import blackHeartIcon from '../../images/blackHeartIcon.svg';
+
+function FavoriteRecipes() {
+  const [recipesFav, setRecipesFav] = useState([]);
+  const [copied, setCopied] = useState(false);
+  const [selectedFilter, setFilter] = useState('all');
+  // const testLocalStorage = () => {
+  //   const favoriteRecipes = [
+  //     {
+  //       id: '52771',
+  //       type: 'meal',
+  //       nationality: 'Italian',
+  //       category: 'Vegetarian',
+  //       alcoholicOrNot: '',
+  //       name: 'Spicy Arrabiata Penne',
+  //       image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+  //     },
+  //     {
+  //       id: '178319',
+  //       type: 'drink',
+  //       nationality: '',
+  //       category: 'Cocktail',
+  //       alcoholicOrNot: 'Alcoholic',
+  //       name: 'Aquamarine',
+  //       image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+  //     },
+  //   ];
+
+  //   localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes));
+  // };
+
+  const handleCopy = (type, id) => {
+    let textToCopy;
+    const countTimeOut = 3000;
+
+    if (type === 'drink') {
+      textToCopy = `http://localhost:3000/drinks/${id}`;
+    }
+    textToCopy = `http://localhost:3000/meals/${id}`;
+
+    navigator.clipboard.writeText(textToCopy)
+      .then(() => {
+        setCopied(true);
+        setTimeout(() => {
+          setCopied(false);
+        }, countTimeOut);
+        console.log('Link copied!');
+      })
+      .catch((error) => {
+        console.error('Erro ao copiar o texto:', error);
+      });
+  };
+
+  const handleRemove = (id) => {
+    const favRecipesData = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+
+    const newFavRecipes = favRecipesData.filter((recipe) => recipe.id !== id);
+
+    localStorage.setItem('favoriteRecipes', JSON.stringify(newFavRecipes));
+  };
+
+  useEffect(() => {
+    // testLocalStorage();
+    const favRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+    setRecipesFav(favRecipes);
+  }, []);
+
+  useEffect(() => {
+    const favRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+    setRecipesFav(favRecipes);
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <Button
+        value="All"
+        test="filter-by-all-btn"
+        onClick={ () => setFilter('all') }
+      />
+      <Button
+        value="Meals"
+        test="filter-by-meal-btn"
+        onClick={ () => setFilter('meal') }
+      />
+      <Button
+        value="Drinks"
+        test="filter-by-drink-btn"
+        onClick={ () => setFilter('drink') }
+      />
+      { recipesFav.length
+        && (
+          <section>
+            { recipesFav.filter((recipe) => {
+              if (selectedFilter === 'all') {
+                return true;
+              }
+              return recipe.type === selectedFilter;
+            })
+              .map((recipe, index) => (
+                <div key={ index }>
+                  <Link
+                    to={ recipe.type === 'drink'
+                      ? `/drinks/${recipe.id}`
+                      : `/meals/${recipe.id}` }
+                  >
+                    <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
+                    <img
+                      src={ recipe.image }
+                      alt=""
+                      data-testid={ `${index}-horizontal-image` }
+                    />
+                  </Link>
+                  <p
+                    data-testid={ `${index}-horizontal-top-text` }
+                  >
+                    { recipe.type !== 'drink'
+                      ? `${recipe.nationality} - ${recipe.category}`
+                      : `${recipe.alcoholicOrNot}`}
+
+                  </p>
+                  <Button
+                    value={
+                      <img
+                        src={ shareIcon }
+                        alt="Compartilhar"
+                        data-testid={ `${index}-horizontal-share-btn` }
+                      />
+                    }
+                    onClick={ () => handleCopy(recipe.type, recipe.id) }
+                  />
+                  <Button
+                    value={
+                      <img
+                        src={ blackHeartIcon }
+                        alt="Favoritar"
+                        data-testid={ `${index}-horizontal-favorite-btn` }
+                      />
+                    }
+                    onClick={ () => handleRemove(recipe.id) }
+                  />
+                  { copied && <p>Link copied!</p>}
+                </div>
+              ))}
+          </section>
+        )}
+    </div>
+  );
+}
+
+export default FavoriteRecipes;
